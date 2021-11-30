1a0f556bc00beadeef2eebe1134efcd9ceeb32a9
JoshPrakke <Joshua.Prakke@capco.com>
2021-11-30 14:30:33 -0500

    Added base guest list array and exit functionality

--- src/Main.java
+++ src/Main.java
@@ -2,25 +2,30 @@ import java.util.Scanner;
 
 public class Main {
     static Scanner sc = new Scanner(System.in);
+    static String[] guestList = new String[1000];
+    static boolean continueScript = true;
     public static void main(String[] args) {
         run();
     }
     public static void run(){
-        System.out.println("1 - Display All Guests");
-        System.out.println("2 - Add Guests");
-        System.out.println("3 - Remove Guests");
-        while(true){
-            System.out.println("\n Make a Selection: ");
-            int optionSelect = sc.nextInt();
-            if(!selector(optionSelect)){
-                break;
+        do {
+            System.out.println("\n1 - Display All Guests");
+            System.out.println("2 - Add Guests");
+            System.out.println("3 - Remove Guests");
+            System.out.println("4 - Quit Program");
+            while (true) {
+                System.out.println("\n Make a Selection: ");
+                int optionSelect = sc.nextInt();
+                if (!selector(optionSelect)) {
+                    break;
+                }
             }
-        }
+        } while (continueScript);
     }
     public static boolean selector(int input){
         switch (input){
             case 1:
-                System.out.println("you selected option 1");
+                allGuests(guestList);
                 return false;
             case 2:
                 System.out.println("you selected option 2");
@@ -28,9 +33,18 @@ public class Main {
             case 3:
                 System.out.println("you selected option 3");
                 return false;
+            case 4:
+                System.out.println("Goodbye...");
+                continueScript = false;
+                return false;
             default:
                  System.out.println("You did not select a valid option.");
                  return true;
+        }
+    }
+    public static void allGuests(String[] arr){
+        for(String name : arr){
+            System.out.println(name);
         }
     }
 }
